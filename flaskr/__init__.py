1b900fd8592ca00b813f9be43d9265bcc945e3d1
Zheng <909956683@qq.com>
2020-03-26 21:52:48 +0800

    new

---
+++ flaskr/__init__.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask, redirect, url_for
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    if not os.path.exists(app.instance_path):
+        os.makedirs(app.instance_path)
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World！'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    return app
+
+
+
